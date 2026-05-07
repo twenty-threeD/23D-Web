ebaf5b10119adf09c6c1ab4eb9b8049d0823ed76
AN JAE MIN <jaeminan944@icloud.com>
2026-05-07 20:05:09 +0900

    feat :: 로그인페이지 디자인 완성

---
+++ app/logIn/signin/page.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+
+import LogoImg from "@/src/img/Logo.svg";
+import ShowPassWD from "@/src/img/logIn/showPassWD.svg";
+import GoogleIMG from "@/src/img/logIn/Google.svg";
+import KakaoTalkIMG from "@/src/img/logIn/kakaoTalk.svg";
+import NaverIMG from "@/src/img/logIn/naver.svg";
+
+import { InputField } from "@/src/components/inputField";
+
+const Page = () => {
+    return (
+        <div className = "bg-[#F4F4F4]">
+            <div className="w-200 h-150  m-auto rounded-3xl flex items-left">
+                <div className="w-100 h-150 bg-linear-to-tr from-[#FF4D4D] to-[#824DFF] rounded-l-3xl flex items-center justify-center"></div>
+                    <div className="w-100 h-150 bg-[#FFFFFF] rounded-r-3xl flex flex-col items-center justify-center">                    
+                        <Image src={LogoImg} alt="Logo" width={80} height={40} className="mb-15.25" />
+
+                        {/* 인풋 필드 */}
+                        <div className="mb-2.5">
+                            <InputField label="아이디 입력" placeholder="아이디를 입력해주세요" type="text" />
+                            <InputField label="비밀번호 입력" placeholder="비밀번호를 입력해주세요" type="password" icon={ShowPassWD} />
+                        </div>
+
+                        {/* 로그인 버튼과 링크 */}
+                        <button className="w-75 h-10
+                        bg-[#FE6A4C] text-white rounded-lg text-lg font-bold hover:bg-[#FE6A4C]/90 transition-colors">
+                            로그인
+                        </button>
+                        <div className="flex justify-between w-75 mt-2.5">
+                            <a href="#" className="text-[10px] text-[#666666] underline">
+                                비밀번호를 잊으셨나요?
+                            </a>
+
+                            <div className="flex gap-1 text-[10px] text-[#666666]">
+                                <span>계정이 없으신가요?</span>
+                                <a href="#" className="underline">회원가입</a>
+                            </div>
+                        </div>
+
+                        {/* 소셜 로그인 */}
+                        <div className="flex gap-4 mt-5">
+                            <button className="w-10 h-10 bg-[#FFFFFF] rounded-full flex items-center justify-center border 
+                            border-[#D9D9D9] hover:border-[#FE6A4C] transition-colors">
+                                <Image src={GoogleIMG} alt="Google" width={20} height={20} />
+                            </button>
+                            <button className="w-10 h-10 bg-[#FFFFFF] rounded-full flex items-center justify-center border 
+                            border-[#D9D9D9] hover:border-[#FE6A4C] transition-colors">
+                                <Image src={KakaoTalkIMG} alt="KakaoTalk" width={20} height={20} />
+                            </button>
+                            <button className="w-10 h-10 bg-[#FFFFFF] rounded-full flex items-center justify-center border 
+                            border-[#D9D9D9] hover:border-[#FE6A4C] transition-colors">
+                                <Image src={NaverIMG} alt="Naver" width={20} height={20} />
+                            </button>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    )
+};
+
+export default Page;
---
+++ src/components/inputField.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useState } from "react";
+import Image from "next/image";
+
+interface InputProps {
+    label: string;
+    placeholder: string;
+    type?: string;
+    icon?: any;
+}
+
+export const InputField = ({ label, placeholder, type = "text", icon }: InputProps) => {
+  const [inputType, setInputType] = useState(type);
+
+  const togglePassword = () => {
+    setInputType((prev) => (prev === "password" ? "text" : "password"));
+  };
+
+  return (
+    <div className="mb-4">
+      <div className="group flex flex-col relative">
+        <h1 className="text-[10px] font-light text-black 
+                      group-focus-within:text-[#FE6A4C] transition-colors"> 
+          {label}
+        </h1>
+        
+        <input 
+          type={inputType}
+          placeholder={placeholder} 
+          className="w-75 h-9.5 border-b-2 border-black outline-none px-3
+                    text-black focus:text-[#FE6A4C] 
+                    focus:border-[#FE6A4C]
+                    placeholder:text-[14px] transition-colors
+                    pr-10"
+        />
+
+        {icon && (
+          <div 
+            className="absolute right-2 bottom-1.5 cursor-pointer hover:opacity-70"
+            onClick={togglePassword}
+          >
+            <Image src={icon} alt="toggle visibility" width={20} height={20} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
